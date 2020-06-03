654c377e7054d43b1cd1c0bd55eb91fe16ac7be1
holi <hao.li@live.cn>
2020-06-03 19:30:23 +0800

    smooth reload

--- Assets/NaughtyCharacter/Scripts/CharacterAnimator.cs
+++ Assets/NaughtyCharacter/Scripts/CharacterAnimator.cs
@@ -19,6 +19,9 @@ namespace NaughtyCharacter
 	{
 		private Animator _animator;
 		private Character _character;
+		private float _currentUpperbodyWeight;
+		private float _targetUpperbodyWeight;
+		private float _currentDampVelocity;
 
 		private void Awake()
 		{
@@ -26,10 +29,17 @@ namespace NaughtyCharacter
 			_character = GetComponent<Character>();
 		}
 
+		public void SetUpperBodyWeight(float weight)
+		{
+			_targetUpperbodyWeight = weight;
+		}
+
 		public void UpdateState()
 		{
-			float normHorizontalSpeed = _character.HorizontalVelocity.magnitude / _character.MovementSettings.MaxRunSpeed;
-			_animator.SetFloat(CharacterAnimatorParamId.HorizontalSpeed, normHorizontalSpeed);
+			_currentUpperbodyWeight = Mathf.SmoothDamp(_currentUpperbodyWeight, _targetUpperbodyWeight, ref _currentDampVelocity, 0.2f);
+			_animator.SetLayerWeight(1, _currentUpperbodyWeight);
+
+			Debug.LogWarning("set weight: " + _currentUpperbodyWeight + "," + _currentDampVelocity);
 
             Vector3 velocity = _character.HorizontalVelocity;
             float moveForward = Vector3.Dot(_character.transform.forward, velocity);
@@ -38,9 +48,6 @@ namespace NaughtyCharacter
             _animator.SetFloat(CharacterAnimatorParamId.MoveForward, moveForward, 0.15f, Time.deltaTime);
             _animator.SetFloat(CharacterAnimatorParamId.MoveRight, moveRight, 0.15f, Time.deltaTime);
 
-			float jumpSpeed = _character.MovementSettings.JumpSpeed;
-			float normVerticalSpeed = _character.VerticalVelocity.y.Remap(-jumpSpeed, jumpSpeed, -1.0f, 1.0f);
-			_animator.SetFloat(CharacterAnimatorParamId.VerticalSpeed, normVerticalSpeed);
             _animator.SetInteger(CharacterAnimatorParamId.MovementState, (int)_character.MovementState);
 
             _animator.SetInteger(CharacterAnimatorParamId.UpperState, (int)_character.UpperState);
